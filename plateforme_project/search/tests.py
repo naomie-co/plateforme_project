1434e968ae7d649eb52f80e9f1f1de4c61db8ed8
Naomie <naomie.colombo@gmail.com>
2020-04-22 23:13:55 +0200

    First functional test attempt

--- plateforme_project/search/tests.py
+++ plateforme_project/search/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from search.models import categorie, op_food
+from selenium import webdriver
 
 """Tests views and the database models"""
 
@@ -97,3 +98,31 @@ class DataBaseTestCase(TestCase):
         new_categorie = categorie.objects.count()
         self.assertEqual(new_categorie, old_categorie + 1)
 
+
+class SearchText(TestCase):
+    def setUp(self):
+        # create a new Firefox session
+        self.driver = webdriver.Firefox(executable_path=r'C:\\Program Files\\geckodriver\\geckodriver.exe')
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window()
+        # navigate to the application home page
+        self.driver.get("http://www.google.com/")
+
+    def test_search_by_text(self):
+        # get the search textbox
+        self.search_field = self.driver.find_element_by_name("q")
+
+        # enter search keyword and submit
+        self.search_field.send_keys("Selenium WebDriver Interview questions")
+        self.search_field.submit()
+
+        #get the list of elements which are displayed after the search
+        #currently on result page usingfind_elements_by_class_namemethod
+
+        lists = self.driver.find_elements_by_class_name("r")
+        no=len(lists)
+        self.assertEqual(11, len(lists))
+
+    def tearDown(self):
+        # close the browser window
+        self.driver.quit()
